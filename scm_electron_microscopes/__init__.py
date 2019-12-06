adced6b73e8e47e9f8af1ec5b3a1b5e6b39e63af
MaartenBransen <54527765+MaartenBransen@users.noreply.github.com>
2019-12-06 11:16:45 +0100

    Update __init__.py

--- scm_electron_microscopes/__init__.py
+++ scm_electron_microscopes/__init__.py
@@ -2,12 +2,14 @@ import cv2
 import numpy as np
 
 from .tem import tecnai
-from .sem import helios,phenom,xls30feg
+from .sem import helios,phenom,xl30sfeg
+from .utility import util
 
 #make visible for 'from stackscroller import *'
 __all__ = [
     'tecnai',
     'helios',
     'phenom',
-    'xls30feg',
+    'xl30sfeg',
+    'util'
 ]
