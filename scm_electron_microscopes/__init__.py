e9a55131e52839c0887b6826b9dc06bb6c8d8206
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2020-09-09 15:54:35 +0200

    create talos alias

--- scm_electron_microscopes/__init__.py
+++ scm_electron_microscopes/__init__.py
@@ -2,6 +2,10 @@ from .tem import tecnai
 from .sem import helios,phenom,xl30sfeg
 from .utility import util
 
+#make talos alias for tecnai/talos class
+talos = technai
+talos.__name__ = 'talos'
+
 #make visible for 'from stackscroller import *'
 __all__ = [
     'tecnai',
