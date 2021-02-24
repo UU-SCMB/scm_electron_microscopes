d6832033126dc96691fb01c4f69935dde6d69610
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2021-02-24 13:07:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scm_electron_microscopes",
+    version="1.0.0",
+    author="Maarten Bransen",
+    author_email="m.bransen@uu.nl",
+    license='GNU General Public License v3.0',
+    long_description=open('README.md').read(),
+    packages=find_packages(include=["scm_electron_microscopes", "scm_electron_microscopes.*"]),
+    install_requires=[
+        "numpy>=1.19.2",
+        "scipy>=1.6.0",
+    ],
+)
